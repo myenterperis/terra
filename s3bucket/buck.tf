aa8b78b3abe46163c10f00d73b0805c6cef7ee52
musiliu yaya <musiyaya12@gmail.com>
2023-04-11 22:44:52 -0400

    my first commit

---
+++ s3bucket/buck.tf
@@ -0,0 +1,45 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+# Configure the AWS Provider
+provider "aws" {
+  version = "~> 4.0"
+  region  = "us-east-1"
+  profile = "default"
+}
+# Get latest AMI ID for Amazon Linux2 OS
+data "aws_ami" "amzlinux2" {
+  most_recent      = true
+  owners           = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-gp2"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+resource "aws_s3_bucket" "buc" {
+  bucket = "tfbucketyayabk29"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+}
